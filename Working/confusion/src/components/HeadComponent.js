72467442d09fcc7caf7ecb25a0f6ccb0a02fb221
baonghi1201 <44896400+baonghi1201@users.noreply.github.com>
2020-08-20 12:29:14 +0300

    Uncontrolled Form

--- Working/confusion/src/components/HeadComponent.js
+++ Working/confusion/src/components/HeadComponent.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron} from 'reactstrap';
+import {Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron,
+        Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label} from 'reactstrap';
 import {NavLink} from 'react-router-dom';
 
 class Header extends Component{
@@ -7,9 +8,13 @@ class Header extends Component{
         super(props);
         
         this.state={
-            isNavOpen: false
+            isNavOpen: false,
+            isModelOpen: false
         };
         this.toggleNav=this.toggleNav.bind(this);
+        this.toggleModal=this.toggleModal.bind(this);
+        this.handleLogin=this.handleLogin.bind(this);
+        
     }
 
     toggleNav(){
@@ -17,6 +22,21 @@ class Header extends Component{
             isNavOpen:!this.state.isNavOpen
         })
     }
+
+
+    toggleModal(){
+        this.setState({
+            isModelOpen:!this.state.isModelOpen
+        })
+    }
+
+    handleLogin(event){
+        this.toggleModal();
+        alert("Username" + this.username.value + "Password" + this.password.value
+                + "Remember me" + this.remember.checked)
+        event.preventDefault();
+    }
+
     render(){
         return(
             <React.Fragment>
@@ -48,6 +68,13 @@ class Header extends Component{
                                 <span className="fa fa-address-card fa-lg"></span> Contact Us </NavLink>
                         </NavItem>
                     </Nav>
+                    <Nav className='ml-auto' navbar>
+                        <NavItem>
+                            <Button outline onClick={this.toggleModal}>
+                                <span className="fa fa-sign-in fa-lg">Login</span>
+                            </Button>
+                        </NavItem>
+                    </Nav>
                     </Collapse>
                 </div>
                 </Navbar>
@@ -59,9 +86,36 @@ class Header extends Component{
                                 <p>We take inspiration from the World's best cuisines, and create a unique fusion experience. Our lipsmacking creations will tickle your culinary senses!</p>
                             </div>
                         </div>
-
                     </div>
                 </Jumbotron>
+
+                <Modal isOpen={this.state.isModelOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                        <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor='username'>Username</Label>
+                                <Input type='text' id='username' name='username'
+                                innerRef={(input) => this.username=input}/>
+                            </FormGroup>
+
+                            <FormGroup>
+                                <Label htmlFor='password'>Password</Label>
+                                <Input type='password' id='password' name='password'
+                                innerRef={(input)=> this.password=input}/>
+                            </FormGroup>
+                            
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type='checkbox' name='remember'
+                                    innerRef={(input)=> this.remember=input}/>Remember me
+                                </Label>                               
+                            </FormGroup>
+
+                            <Button type='submit' value='submit' color='primary'>Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
             </React.Fragment>
         );
     }
