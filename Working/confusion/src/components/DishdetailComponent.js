6be72924a101a14c5759dbcc54ed2b84642d4868
baonghi1201 <44896400+baonghi1201@users.noreply.github.com>
2020-08-23 17:46:52 +0300

    Assignment 3

--- Working/confusion/src/components/DishdetailComponent.js
+++ Working/confusion/src/components/DishdetailComponent.js
@@ -8,116 +8,113 @@ const required= (val) => val && val.length;
 const maxLength= (len) => (val) => !(val) || (val.length <=len);
 const minLength= (len) => (val) => (val) && (val.length >=len);    
 
-class CommentForm extends Component{
+class CommentForm extends Component {
     constructor(props) {
-        super(props);
-
-        this.state = {
-          isModalOpen: false
-        };
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+      super(props);
+      this.state = {
+        isModalOpen: false
+      };
+      this.toggleModal = this.toggleModal.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
-    
-      toggleModal() {
-        this.setState({
-          isModalOpen: !this.state.isModalOpen
-        });
+  
+    toggleModal() {
+      this.setState({
+        isModalOpen: !this.state.isModalOpen
+      });
     }
-    
-      handleSubmit(values) {
-        this.toggleModal();
-        this.props.postComment(
-          this.props.dishId,
-          values.rating,
-          values.author,
-          values.comment
-        );
+  
+    handleSubmit(values) {
+      this.toggleModal();
+      this.props.postComment(
+        this.props.dishId,
+        values.rating,
+        values.author,
+        values.comment
+      );
     }
-    
-      render() {
-        return (
-          <div className='container'>
-              <div className='row'>
+  
+    render() {
+      return (
+        <div>
             <Button outline onClick={this.toggleModal}>
-              <span className="fa fa-pencil" /> Submit Comment
+            <span className="fa fa-pencil" /> Submit Comment
             </Button>
             <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-              <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
-              <ModalBody>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
                 <LocalForm onSubmit={this.handleSubmit}>
-                  <Row className="form-group">
+                <Row className="form-group">
                     <Label htmlFor="rating" md={12}>
-                      Rating
+                    Rating
                     </Label>
                     <Col md={{ size: 12 }}>
-                      <Control.select
+                    <Control.select
                         model=".rating"
                         name="rating"
                         className="form-control"
-                      >
+                    >
                         <option>1</option>
                         <option>2</option>
                         <option>3</option>
                         <option>4</option>
                         <option>5</option>
-                      </Control.select>
+                    </Control.select>
                     </Col>
-                  </Row>
-                  <Row className="form-group">
+                </Row>
+                <Row className="form-group">
                     <Label htmlFor="author" md={12}>
-                      Your Name
+                    Your Name
                     </Label>
                     <Col md={12}>
-                      <Control.text
+                    <Control.text
                         model=".author"
                         id="author"
                         name="author"
                         placeholder="Your Name"
                         className="form-control"
                         validators={{
-                          required,
-                          minLength: minLength(3),
-                          maxLength: maxLength(15)
+                        required,
+                        minLength: minLength(3),
+                        maxLength: maxLength(15)
                         }}
-                      />
-                      <Errors
+                    />
+                    <Errors
                         className="text-danger"
                         model=".author"
                         show="touched"
                         messages={{
-                          required: "Required",
-                          minLength: "Must be greater than 2 characters",
-                          maxLength: "Must be 15 characters or less"
+                        required: "Required",
+                        minLength: "Must be greater than 2 characters",
+                        maxLength: "Must be 15 characters or less"
                         }}
-                      />
+                    />
                     </Col>
-                  </Row>
-                  <Row className="form-group">
+                </Row>
+                <Row className="form-group">
                     <Label htmlFor="comment" md={12}>
-                      Comment
+                    Comment
                     </Label>
                     <Col md={12}>
-                      <Control.textarea
+                    <Control.textarea
                         model=".comment"
                         id="comment"
                         name="comment"
                         rows={5}
                         className="form-control"
-                      />
+                    />
                     </Col>
-                  </Row>
-                  <Button type="submit" value="submit" color="primary">
+                </Row>
+                <Button type="submit" value="submit" color="primary">
                     Submit
-                  </Button>
+                </Button>
                 </LocalForm>
-              </ModalBody>
+            </ModalBody>
             </Modal>
-            </div>
-          </div>
-        );
+        </div>
+      );
     }
-}
+  }
 function RenderComments({comments}){
         if (comments == null){
             return (<div></div>)
